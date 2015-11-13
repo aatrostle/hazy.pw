33851312040ba16b6e2acc18da2ad722a92885d2
Aaron Trostle <aaron.trostle@gmail.com>
2015-11-12 23:53:04 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HelloPhoenix.Mixfile do
 
   def project do
     [app: :hello_phoenix,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
